3199be35358f0fea5863682ad4e0df741c27b224
Garth Lilly <garff247@gmail.com>
2019-04-21 22:05:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,11 +87,11 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto] << "hi"
-		# programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 #programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
-		# programmer_hash[:yukihiro_matsumoto][:languages][1] = "C"
-		 #return programmer_hash
+		 programmer_hash[:yukihiro_matsumoto] = {
+			 :known_for => "Ruby",
+			 :languages => ["LISP", "C"]
+		 }
+		 return programmer_hash
 end
 
 def changing_alan
